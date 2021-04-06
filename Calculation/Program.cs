d38863de7aaf62c15e395869450bd005490860bd
BlackVovan <vladik123456022@gmail.com>
2021-04-06 03:01:21 +0300

    Add project files.

---
+++ Calculation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Calculation
+{
+    public static class Program
+    {
+        public static int FoundMax2nd_Original(int[] inputArray)
+        {
+            if (inputArray.Length <= 1)
+            {
+                throw new ArgumentException("Array lenth is equal to zero or too shor for identifiying second large integer.", nameof(inputArray));
+            }
+
+            if (inputArray == null)
+            {
+                throw new NullReferenceException(nameof(inputArray));
+            }
+
+            foreach (int number in inputArray)
+            {
+                if (number == int.MaxValue || number == int.MinValue)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(number), "Argument is equal to Int.MinValue or Int.MinValue.");
+                }
+            }
+
+            int buffer;
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                for (int j = 0; j < inputArray.Length - i - 1; j++)
+                {
+                    if (inputArray[j] > inputArray[j + 1])
+                    {
+                        buffer = inputArray[j];
+                        inputArray[j] = inputArray[j + 1];
+                        inputArray[j + 1] = buffer;
+                    }
+                }
+            }
+
+            return inputArray[^1];
+        }
+    }
+}
---
+++ Calculations.Tests/UnitTest1.cs
@@ -0,0 +1,35 @@
+using System;
+using NUnit.Framework;
+using static Calculation.Program;
+
+namespace Calculations.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase(new int[] { 0, 4, 6, 25, 67, 101, 3 }, ExpectedResult = 101)]
+        [TestCase(new int[] { 0, 4, 6, 4, 10, 3 }, ExpectedResult = 10)]
+        [TestCase(new int[] { 0, 4, 6, -10, 25, 3, 1, 3 }, ExpectedResult = 25)]
+        [TestCase(new int[] { 0, 4, 6, 67, 1, 3 }, ExpectedResult = 67)]
+        [TestCase(new int[] { 0, 4, 6, 5, 2, 6, 0, 3 }, ExpectedResult = 6)]
+        [TestCase(new int[] { 67, 89, 3 }, ExpectedResult = 89)]
+        public int GetMax2nd_original(params int[] inputArray) => FoundMax2nd_Original(inputArray);
+
+        [Test]
+        public void FoundMax2nd_Original_WithOneNumber_ThrowArgumentException() =>
+            Assert.Throws<ArgumentException>(() => FoundMax2nd_Original(new int[] { 1 }), "Array lenth is equal to zero or too shor for identifiying second large integer.");
+
+        [TestCase(new int[] { int.MinValue, 0 })]
+        [TestCase(new int[] { int.MinValue, int.MinValue })]
+        [TestCase(new int[] { 0, int.MinValue })]
+        [TestCase(new int[] { int.MaxValue, 0 })]
+        [TestCase(new int[] { int.MaxValue, int.MaxValue })]
+        [TestCase(new int[] { 0, int.MaxValue })]
+        public void FoundMax2nd_Original_WithOneOrTwoMinOrMaxIntegers_ThrowArgumentOutOfRangeException(params int[] inputArray) =>
+            Assert.Throws<ArgumentOutOfRangeException>(() => FoundMax2nd_Original(inputArray), $"Number cannot be {int.MinValue}.");
+
+        [Test]
+        public void FoundMax2nd_Original_WithNulArray_ThrowArgumentNullException() =>
+            Assert.Throws<NullReferenceException>(() => FoundMax2nd_Original(null));
+    }
+}
